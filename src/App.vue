e1dcb1406ed626c82d69abca3b813b42ed8d3fba
candiaruben2 <candiaruben2@gmail.com>
2020-12-07 00:29:02 -0500

    Commit Componentes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,66 @@
 <template>
   <div id="app">
+    <div id="team" class="container-fluid">
+      <div class="row justify-content-center mb-5">
+        <div class="col mt-5 h-100" v-for="(item, index) of team" :key="index">
+          <team-card :member="item"></team-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import TeamCard from "./components/TeamCard.vue";
 
 export default {
-  name: 'App',
-}
+  name: "App",
+  components: {
+    TeamCard,
+  },
+  data() {
+    return {
+      team: [
+        {
+          codigo: 1,
+          nombre: "David Felipe Varón Molano",
+          descripcion:
+            "Estudiante de ingeniería mecánica de la Universidad Nacional de colombia, curioso por la programación, aficionado a los videojuegos y con ganas por mejorar la educación en colombia.",
+          rol: "Developement Team",
+          imagen: "/assets/davidvaron.jpg",
+        },
+        {
+          codigo: 2,
+          nombre: "Julián Andrés López Quintero",
+          descripcion: "Director de orquesta y compositor de musica para cine, TV y videojuegos; apasionado por el baile, aficionado a la programacion y a la cocina, dispuesto a aprender algo distinto cada dia y compartir el conocimiento.",
+          rol: "Developement Team",
+          imagen: "/assets/julianlopez.jpg",
+        },
+        {
+          codigo: 3,
+          nombre: "Rubén Darío Candia Parra",
+          descripcion: "Ingeniero Industrial y Especialista en gestión de proyectos, apasionado de la lectura, dibujo y el aprendizaje.",
+          rol: "Developement Team",
+          imagen: "/assets/rubencandia.jpg",
+        },
+        {
+          codigo: 4,
+          nombre: "Antonio Morales Correa",
+          descripcion: "Tecnólogo en Sistemas de Telecomunicaciones, apasionado de la lectura y videojuegos, en busca de nuevo conocimiento en programación y otras ramas de ella.",
+          rol: "Developement Team",
+          imagen: "/assets/antoniomorales.jpg",
+        },
+        {
+          codigo: 5,
+          nombre: "No Activo",
+          descripcion:
+            "Estudiante no activo.",
+          rol: "Developement Team",
+          imagen: "/assets/nn.webp",
+        },
+      ],
+    };
+  },
+};
 </script>
 
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,16 +1,25 @@
 <template>
-    <div>
-
+  <div>
+      <div class="card bg-dark text-white">
+        <img class="card-img-top" style="height: 200px; object-fit: cover; width: 100%" :src="member.imagen" alt="Card image cap">
+        <div class="card-body">
+          <h6 class="card-title text-center">{{ member.nombre }}</h6>
+          <p class="card-text" style="font-size: 12px">{{ member.descripcion }}</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-light">{{ member.rol }}</small> <br>
+          <small class="text-light">Código {{ member.codigo }}</small>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "TeamCard",
-        props: ['member']
-    }
+export default {
+  name: "TeamCard",
+  props: ["member"],
+};
 </script>
 
 <style scoped>
-
 </style>
